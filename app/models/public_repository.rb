8ae7f80baf3bb67b4861d4d27f174e49922278d8
Shaq <Shaq.Hosseini@gmail.com>
2014-06-13 14:30:14 +0200

    database constraint and home page design changed

--- app/models/public_repository.rb
+++ app/models/public_repository.rb
@@ -4,5 +4,5 @@ validates :name, length: { maximum: 45 },presence: true,uniqueness: true
 validates :url, length: { maximum: 1000 },presence: true,uniqueness: true
 validates :record_id, length: { maximum: 45 },presence: true,uniqueness: true
 	belongs_to :publication
-end
+	end
 end
